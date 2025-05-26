353b77d4ddd57fcd33fd415c51f5650c124f2ca5
David Kipkemoi <davidkipkemoi30@gmail.com>
2025-05-26 22:06:46 +0300

    Setup hardhat for smart contracts

---
+++ smart-contracts/contracts/NFT.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(string memory name, string memory symbol, address initialOwner) 
+        ERC721(name, symbol)
+        Ownable(initialOwner)
+    {}
+
+    function mint(address to, string memory uri) 
+        public 
+        onlyOwner
+        returns (uint256) 
+    {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        return tokenId;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint256 amount) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, amount);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+} 
